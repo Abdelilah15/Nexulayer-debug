7723f72e3d783100b7ec1015716b49224a0f3527
Abdelilah Moustakim <mosmoustakim50@gmail.com>
2026-06-22 19:41:13 +0100

    Mise à jour

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client';
-import { PrismaNeon } from '@prisma/adapter-neon';
 import { Pool, neonConfig } from '@neondatabase/serverless';
+import { PrismaNeon } from '@prisma/adapter-neon';
 import ws from 'ws';
 
 neonConfig.webSocketConstructor = ws;
@@ -9,20 +9,16 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-// L'initialisation est enveloppée dans une fonction
 export const prisma = globalForPrisma.prisma ?? (() => {
-  
-  // 1. On lit l'URL ICI (au moment de l'exécution de la requête, pas au chargement du fichier)
   const dbUrl = process.env.DATABASE_URL;
-
-  // 2. Sécurité : On fait crasher proprement si Next.js n'a pas lu le .env
+  
   if (!dbUrl) {
-    throw new Error("🚨 CRITIQUE : DATABASE_URL est introuvable. Next.js n'a pas chargé le fichier .env !");
+    throw new Error("🚨 CRITIQUE : DATABASE_URL est introuvable.");
   }
 
-  // 3. Initialisation du Pool avec la certitude que dbUrl existe
+  // Prisma v7 exige l'adaptateur Neon
   const pool = new Pool({ connectionString: dbUrl });
-  const adapter = new PrismaNeon(pool as any);
+  const adapter = new PrismaNeon(pool);
   
   return new PrismaClient({ adapter });
 })();
